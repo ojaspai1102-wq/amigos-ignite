4a512abe833a0196b0d5ff9db039e363a1987ede
ojaspai1102-wq <ojaspai1102@gmail.com>
2026-09-03 19:25:18 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.CAREERMITRA_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://ncyuzxqjplfdigayfsha.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jeXV6eHFqcGxmZGlnYXlmc2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MzYzODIsImV4cCI6MjEwNDAxMjM4Mn0.QRM8wQMN-iqWXZgV14BbMMAHejIugJJkr47iKUJwudU",
 };
